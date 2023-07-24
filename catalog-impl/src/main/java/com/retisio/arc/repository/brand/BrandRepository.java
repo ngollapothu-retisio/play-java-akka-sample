6066a53d7f77e804af7d039cc053c7a8fa004909
Nagamohan Gollapothu <ngollapothu@retisio.com>
2023-07-24 09:23:50 +0530

    r2dbcSession usage is added in repository layer

--- catalog-impl/src/main/java/com/retisio/arc/repository/brand/BrandRepository.java
+++ catalog-impl/src/main/java/com/retisio/arc/repository/brand/BrandRepository.java
@@ -1,6 +1,9 @@
 package com.retisio.arc.repository.brand;
 
 import akka.Done;
+import akka.actor.ActorSystem;
+import akka.actor.typed.javadsl.Adapter;
+import akka.projection.r2dbc.javadsl.R2dbcSession;
 import com.retisio.arc.message.brand.BrandMessage;
 import com.retisio.arc.r2dbc.R2dbcConnectionFactroyWrapper;
 import com.retisio.arc.r2dbc.StatementWrapper;
@@ -15,6 +18,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,11 +27,20 @@ public class BrandRepository {
 
     @Inject
     private R2dbcConnectionFactroyWrapper connectionFactoryWrapper;
+    private final akka.actor.typed.ActorSystem<Void> typedActorSystem;
 
+    @Inject
+    public BrandRepository(ActorSystem classicActorSystem){
+        typedActorSystem = Adapter.toTyped(classicActorSystem);
+    }
+
+    private R2dbcSession getR2dbcSession(Connection connection){
+        return new R2dbcSession(connection, typedActorSystem.executionContext(), typedActorSystem);
+    }
     public CompletionStage<Done> saveBrand(BrandMessage.Brand brand) {
         return Mono.usingWhen(connectionFactoryWrapper.connectionFactory().create(),
                 connection -> {
-                    return saveBrand(connection, brand)
+                    return saveBrand(getR2dbcSession(connection), brand)
                             .map(result -> Done.getInstance());
                 },
                 connection -> connection.close()).toFuture();
@@ -47,19 +60,17 @@ public class BrandRepository {
             "IS_DELETED=excluded.IS_DELETED, " +
             "LAST_MODIFIED_TMST=now()";
 
-    private Mono<List<Integer>> saveBrand(Connection connection, BrandMessage.Brand brand) {
+    private Mono<Done> saveBrand(R2dbcSession r2dbcSession, BrandMessage.Brand brand) {
         log.info("saveBrand brandId::{}", brand.getBrandId());
         AtomicInteger index = new AtomicInteger(-1);
-        StatementWrapper statementWrapper = new StatementWrapper(connection.createStatement(SAVE_BRAND_QUERY));
+        StatementWrapper statementWrapper = new StatementWrapper(r2dbcSession.createStatement(SAVE_BRAND_QUERY));
         statementWrapper.bind(index.incrementAndGet(), brand.getBrandId(), String.class);
         statementWrapper.bind(index.incrementAndGet(), brand.getBrandName(), String.class);
         statementWrapper.bind(index.incrementAndGet(), Optional.ofNullable(brand.getActive()).orElseGet(()->false), Boolean.class);
         statementWrapper.bind(index.incrementAndGet(), Optional.ofNullable(brand.getDeleted()).orElseGet(()->false), Boolean.class);
         statementWrapper.bind(index.incrementAndGet(), Timestamp.valueOf(LocalDateTime.now()), Timestamp.class);
-        return Flux.from(statementWrapper.getStatement().execute())
-                .flatMap(Result::getRowsUpdated)
-                .onErrorStop()
-                .collectList();
+        return Mono.fromCompletionStage(r2dbcSession.updateOne(statementWrapper.getStatement())
+                .thenApply(integer -> Done.getInstance()));
     }
 
 }
